1c5fd8d3e93d15d1ae0792e0c5a76d1da1ffceb9
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-07 10:20:42 -0500

    feat:creacion de   la base de datos y conversion de los archivos csv

---
+++ bd/bd.js
@@ -0,0 +1,23 @@
+import mysql from "mysql2";
+// importamos a mysql para utilizar, la libreria descargar la libreria de mysql2
+
+// creamos la conexion con mysql
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "prueba",
+});
+
+// probamos la conexion a la base de datos
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error de conexión:", err.message);
+  } else {
+    console.log("Base de datos conectada");
+  }
+});
+
+// exportamos para utilizarla en los servicios
+export default connection;
---
+++ server/server.js
@@ -0,0 +1,36 @@
+
+// desdcargamos la libreria csv-parser para leer los datos en js
+import fs from "fs";
+import csv from "csv-parser";
+// llamamos a la conexion y le agregamos el .js al final 
+import connection from "../bd/bd.js";
+
+
+
+// vamos a leer el archivo csv linea por linea 
+fs.createReadStream("../csv/usuarios.csv")
+// convertimos  cada linea de csv  en objetos js 
+  .pipe(csv())
+  //leemos los datos fila por fila 
+  .on("data", (fila) => {
+    // extraemos los datos que estan en el csv
+    const { nombre, correo, telefono, edad } = fila;
+    const sql =
+    // creamos la queries  de inertar los datos en la tabla 
+      "INSERT INTO usuarios (nombre,correo,telefono,edad) VALUES (?, ?, ?, ?)";
+      //  llamamos a la conexion para que inserte los datos en las filas  
+    connection.query(sql, [nombre, correo, telefono, edad], (err) => {
+      // controlamos los errores  que n de la base de datos 
+      if (err) console.error("erroe al insertar datos", err.message);
+      else console.log("insertado " + nombre);
+    });
+  })
+  .on("end", () => {
+    console.log(" Importación completada.");
+    // le damos tiempo para la insercion y finalizamos la conexion
+
+    setTimeout(() => {
+      connection.end();
+      console.log(" conexion cerrada");
+    }, 700);
+  });
