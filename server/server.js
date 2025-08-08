c83235afab1ed1e330bc4768e499dd7690a39f8b
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-08 08:34:35 -0500

    debe agregar el separador y  seleccionar ecoding

--- server/server.js
+++ server/server.js
@@ -15,13 +15,12 @@ function cargarUsuarios() {
     .on("data", (fila) => {
       // extraemos los datos que estan en el csv
       const { identificacion, nombre, correo, telefono } = fila;
+     
       const sql =
-        // creamos la queries  de inertar los datos en la tabla
+        // creamos la queries  de insertar los datos en la tabla
         "INSERT INTO usuarios (identificacion,nombre,correo,telefono) VALUES (?, ?, ?, ?)";
       //  llamamos a la conexion para que inserte los datos en las filas
-      connection.query(
-        sql,
-        [identificacion, nombre, correo, telefono],
+      connection.query(sql,[identificacion, nombre, correo, telefono],
         (err) => {
           // controlamos los errores  que n de la base de datos
           if (err) console.error("erroe al insertar datos", err.message);
@@ -43,19 +42,51 @@ function cargarUsuarios() {
 function cargarLibros() {
   fs.createReadStream("../csv/libros.csv")
     // convertimos  cada linea de csv  en objetos js   y separador para que identifique
-    .pipe(csv())
+    .pipe(csv({separator:";"}))
     //leemos los datos fila por fila
-    .on("data", (fila) => {
+    .on("data", (row) => {
       // extraemos los datos que estan en el csv
-      const { isbn, titulo, año_de_publicacion, autor } = fila;
-
-      const sql =
-        // creamos la queries  de inertar los datos en la tabla
-        "INSERT INTO libros (isbn,titulo,año_de_publicacion,autor) VALUES ( ?,?,?,? )";
+      const { isbn, titulo, año_de_publicacion, autor } =row;
+       console.log(row)
+ // creamos la queries  de inertar los datos en la tabla
+ 
+      const sql = "INSERT INTO libros (isbn,titulo,año_de_publicacion,autor) VALUES ( ?,?,?,? )";
       //  llamamos a la conexion para que inserte los datos en las filas
-      connection.query(sql, [isbn, titulo, año_de_publicacion, autor], (err) => {
+      connection.query(sql, [isbn, titulo, año_de_publicacion, autor],
+         (err) => {
         // controlamos los errores  que n de la base de datos
-        if (err) console.error("erroe al insertar datos", err.message);
+        if (err) console.error("error al insertar datos", err.message);
+      });
+    })
+    .on("end", () => {
+      console.log(" Importación completada.");
+
+      // le damos tiempo para la insercion y finalizamos la conexion
+
+      setTimeout(() => {
+        connection.end();
+        console.log("*** conexion cerrada ***");
+      }, 700);
+    });
+}
+
+function cargarPrestamos() {
+  fs.createReadStream("../csv/prestamos.csv")
+    // convertimos  cada linea de csv  en objetos js   y separador para que identifique
+    .pipe(csv({separator:";"}))
+    //leemos los datos fila por fila
+    .on("data", (row) => {
+      // extraemos los datos que estan en el csv
+      const { isbn, titulo, año_de_publicacion, autor } =row;
+       console.log(row)
+ // creamos la queries  de inertar los datos en la tabla
+ 
+      const sql = "INSERT INTO libros (isbn,titulo,año_de_publicacion,autor) VALUES ( ?,?,?,? )";
+      //  llamamos a la conexion para que inserte los datos en las filas
+      connection.query(sql, [isbn, titulo, año_de_publicacion, autor],
+         (err) => {
+        // controlamos los errores  que n de la base de datos
+        if (err) console.error("error al insertar datos", err.message);
       });
     })
     .on("end", () => {
