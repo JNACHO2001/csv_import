8b66f392f728fab081e9070e48acbe1eef06aa81
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-07 23:33:23 -0500

    feat:importacion de libros

--- bd/bd_script.sql
+++ bd/bd_script.sql
@@ -15,5 +15,23 @@ describe usuarios;
 truncate usuarios;
 select * from  usuarios;
 
+create table libros (
+isbn varchar(250) not null primary key ,
+titulo varchar(250)  ,
+año_de_publicacion varchar(250),
+autor varchar(250),
+creado timestamp default  current_timestamp,
+actualiza timestamp default current_timestamp  on update current_timestamp
+);
+drop  table libros;
+describe libros;
+truncate libros;
+select * from  libros;
+
+
+
+
+
+
 
 
--- server/server.js
+++ server/server.js
@@ -3,37 +3,69 @@ import fs from "fs";
 import csv from "csv-parser";
 // llamamos a la conexion y le agregamos el .js al final
 import connection from "../bd/bd.js";
-
-cargarUsuarios()
-// creamos una funcion 
+cargarUsuarios
+cargarLibros();
+// creamos una funcion
 function cargarUsuarios() {
   // vamos a leer el archivo csv linea por linea
-fs.createReadStream("../csv/usuarios.csv")
-  // convertimos  cada linea de csv  en objetos js   y separador para que identifique 
-  .pipe(csv({separator:";"}))
-  //leemos los datos fila por fila
-  .on("data", (fila) => {
-    // extraemos los datos que estan en el csv
-    const { identificacion, nombre, correo, telefono } = fila;
-    const sql =
-      // creamos la queries  de inertar los datos en la tabla
-      "INSERT INTO usuarios (identificacion,nombre,correo,telefono) VALUES (?, ?, ?, ?)";
-    //  llamamos a la conexion para que inserte los datos en las filas
-    connection.query(sql, [identificacion,nombre, correo, telefono], (err) => {
-      // controlamos los errores  que n de la base de datos
-      if (err) console.error("erroe al insertar datos", err.message);
-      else console.log("insertado " + nombre);
+  fs.createReadStream("../csv/usuarios.csv")
+    // convertimos  cada linea de csv  en objetos js   y separador para que identifique
+    .pipe(csv({ separator: ";" }))
+    //leemos los datos fila por fila
+    .on("data", (fila) => {
+      // extraemos los datos que estan en el csv
+      const { identificacion, nombre, correo, telefono } = fila;
+      const sql =
+        // creamos la queries  de inertar los datos en la tabla
+        "INSERT INTO usuarios (identificacion,nombre,correo,telefono) VALUES (?, ?, ?, ?)";
+      //  llamamos a la conexion para que inserte los datos en las filas
+      connection.query(
+        sql,
+        [identificacion, nombre, correo, telefono],
+        (err) => {
+          // controlamos los errores  que n de la base de datos
+          if (err) console.error("erroe al insertar datos", err.message);
+          else console.log("insertado " + nombre);
+        }
+      );
+    })
+    .on("end", () => {
+      console.log(" Importación completada.");
+      // le damos tiempo para la insercion y finalizamos la conexion
+
+      setTimeout(() => {
+        connection.end();
+        console.log("*** conexion cerrada ***");
+      }, 700);
     });
-  })
-  .on("end", () => {
-    console.log(" Importación completada.");
-    // le damos tiempo para la insercion y finalizamos la conexion
+}
 
-    setTimeout(() => {
-      connection.end();
-      console.log("*** conexion cerrada ***");
-    }, 700);
-  });
+function cargarLibros() {
+  fs.createReadStream("../csv/libros.csv")
+    // convertimos  cada linea de csv  en objetos js   y separador para que identifique
+    .pipe(csv())
+    //leemos los datos fila por fila
+    .on("data", (fila) => {
+      // extraemos los datos que estan en el csv
+      const { isbn, titulo, año_de_publicacion, autor } = fila;
 
-  
+      const sql =
+        // creamos la queries  de inertar los datos en la tabla
+        "INSERT INTO libros (isbn,titulo,año_de_publicacion,autor) VALUES ( ?,?,?,? )";
+      //  llamamos a la conexion para que inserte los datos en las filas
+      connection.query(sql, [isbn, titulo, año_de_publicacion, autor], (err) => {
+        // controlamos los errores  que n de la base de datos
+        if (err) console.error("erroe al insertar datos", err.message);
+      });
+    })
+    .on("end", () => {
+      console.log(" Importación completada.");
+
+      // le damos tiempo para la insercion y finalizamos la conexion
+
+      setTimeout(() => {
+        connection.end();
+        console.log("*** conexion cerrada ***");
+      }, 700);
+    });
 }
