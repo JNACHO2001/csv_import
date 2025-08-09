dca57ba518aa47030013e0a3e22dc99c0c855533
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-08 19:12:09 -0500

    feat:creamos los metodos get

---
+++ index.js
@@ -0,0 +1,82 @@
+import express from "express";
+import connection from "./bd/bd.js";
+
+const app = express();
+
+app.use(express.json());
+
+/*  GET PARA TODOS LOS PRESTAMOS  */
+
+app.get("/", (req, res) => {
+  try {
+    const query = `
+    SELECT usuarios.nombre AS usuario,
+           libros.titulo AS libro,
+           prestamos.fecha_devolucion
+    FROM prestamos
+    JOIN usuarios
+      ON prestamos.id_usuario = usuarios.id_usuario
+    JOIN libros
+      ON libros.isbn = prestamos.isbn;
+  `;
+
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error("Error en la consulta:", error);
+        return res.status(500).send("Error al obtener datos");
+      }
+      res.json(results);
+    });
+  } catch (error) {
+    res.json({
+      message: "existe un error " + error,
+    });
+  }
+});
+
+app.get("/prestamos/:id_prestamo", async (req, res) => {
+    try { 
+        const id = req.params.id_prestamo 
+
+    const query = `
+    SELECT usuarios.nombre AS usuario,
+           libros.titulo AS libro,
+           prestamos.fecha_devolucion
+    FROM prestamos
+    JOIN usuarios
+      ON prestamos.id_usuario = usuarios.id_usuario
+    JOIN libros
+      ON libros.isbn = prestamos.isbn   WHERE id_prestamo = ? ;
+  `;
+
+    connection.query(query,[id], (error, results) => {
+      if (error) {
+        console.error("Error en la consulta:", error);
+        return res.status(500).send("Error al obtener datos");
+      }
+       if (results.length ===0) {
+        return res.status(404).send("no lo encontre")
+        
+       }
+
+      res.json(results[0])
+    });
+  } catch (error) {
+    res.json({
+      message: "existe un error " + error,
+    });
+  }
+
+
+
+});
+
+
+app.post("/prestamos", async (req,res)  =>{
+    
+} )
+
+
+app.listen(3000, () => {
+  console.log("servidor arriba  en http://localhost:3000");
+});
