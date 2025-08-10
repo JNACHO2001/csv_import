ca68f805bfaa69074b9fcfd0bea2e3156fdbea27
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-09 21:30:35 -0500

    feat:se actualizo las  consultas

--- bd/bd_script.sql
+++ bd/bd_script.sql
@@ -17,7 +17,7 @@ truncate usuarios;
 select * from  usuarios;
 
 create table libros (
-isbn varchar(25) not null primary key ,
+isbn varchar(50) not null primary key ,
 titulo varchar(250)  ,
 año_de_publicacion varchar(250),
 autor varchar(250),
@@ -36,11 +36,11 @@ id_estado int  auto_increment primary key,
 nombre varchar(20) not null
 );
 
-
+select *from prestamos;
 create table prestamos(
 id_prestamo int auto_increment primary key,
 id_estado int,
-isbn varchar(25),
+isbn varchar(50) ,
 id_usuario int,
 fecha_prestamo date,
 fecha_devolucion date,
@@ -51,8 +51,41 @@ creado timestamp default  current_timestamp,
 actualiza timestamp default current_timestamp  on update current_timestamp
 );
 
+update prestamos 
+set id_estado = 2
+where id_prestamo =2;
+
+UPDATE prestamos SET 
+fecha_prestamo =  "2025-05-07" ,fecha_devolucion = "2025-09-07" 
+WHERE id_prestamo = 4;
+    
+    
+UPDATE prestamos SET 
+id_estado = 2,id_usuario = 1,isbn = 978-1-84859-314-5,fecha_prestamo = "2025-08-09",fecha_devolucion =  "2025-08-15"  
+WHERE id_prestamo = 4;
+
+
+
+
+
+
+select  usuarios.nombre  as usuario,libros.titulo  as libro,  prestamos.fecha_devolucion
+from prestamos
+join  usuarios
+on
+prestamos.id_usuario=usuarios.id_usuario
+join libros
+on 
+libros.isbn=prestamos.isbn;
+
 select *from estados;
 truncate estados;
+
+
+
+
+
+
 
 
 
